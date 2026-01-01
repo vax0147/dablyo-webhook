3a6021160b8386e1a235eafed20f42ab4264f38c
Jeff Vass <jefffvasss@gmail.com>
2026-01-01 13:24:10 +0100

    Add Netlify webhook function

---
+++ netlify/functions/webhook.js
@@ -0,0 +1,24 @@
+const handler = async (event) => {
+  const VERIFY_TOKEN = "iVhubyPfGBgfkWAO";
+
+  if (event.httpMethod === "GET") {
+    const params = event.queryStringParameters;
+    if (params['hub.mode'] === 'subscribe' && params['hub.verify_token'] === VERIFY_TOKEN) {
+      return {
+        statusCode: 200,
+        body: params['hub.challenge']
+      };
+    } else {
+      return { statusCode: 403, body: "Forbidden" };
+    }
+  }
+
+  if (event.httpMethod === "POST") {
+    console.log("Webhook event received:", event.body);
+    return { statusCode: 200, body: "EVENT_RECEIVED" };
+  }
+
+  return { statusCode: 405, body: "Method Not Allowed" };
+};
+
+module.exports = { handler };
